3b5ad415a895bc9b440f91a879a879206d0bf193
Leonardo <105111900+TheBoykiss@users.noreply.github.com>
2022-05-27 08:43:06 -0700

    Add files via upload

---
+++ src/main/java/me/theboykiss/ovh/cryjoinmessage/Events/JoinEvents.java
@@ -0,0 +1,43 @@
+package me.theboykiss.ovh.cryjoinmessage.Events;
+
+import me.theboykiss.ovh.cryjoinmessage.CryJoinMessage;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class JoinEvents implements Listener {
+    private CryJoinMessage plugin;
+    public JoinEvents(CryJoinMessage plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void Join2(PlayerJoinEvent e){
+        Player p = e.getPlayer();
+        String messageFromConfig = plugin.getConfig().getString("join-message");
+        if (messageFromConfig.contains("%playername%")) {
+            messageFromConfig = messageFromConfig.replace("%playername%", p.getName());
+        }
+        Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', messageFromConfig));
+        p.sendTitle(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("title-join")), ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("subtitle-join")), 1, 100, 1);
+        p.playSound(p.getLocation(), Sound.valueOf(plugin.getConfig().getString("sound-join")), 5, 5);
+        e.setJoinMessage("");
+    }
+
+    @EventHandler
+    public void Leave2(PlayerQuitEvent e){
+        Player p = e.getPlayer();
+        String messageFromConfig = plugin.getConfig().getString("leave-message");
+        if (messageFromConfig.contains("%playername%")) {
+            messageFromConfig = messageFromConfig.replace("%playername%", p.getName());
+        }
+        Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', messageFromConfig));
+        p.playSound(p.getLocation(), Sound.valueOf(plugin.getConfig().getString("sound-leave")), 5, 5);
+        e.setQuitMessage("");
+    }
+}
